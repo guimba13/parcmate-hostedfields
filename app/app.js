97e9c74f2a8ab3f498146d2d423400f83cbf65be
Guilherme Zanelato <zanelato@localhostsro.sk>
2015-12-01 15:24:33 +0100

    fixing server url

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 //var server = "http://localhost:9010/";
-var server = "http://server.localhostsro.sk/9010/";
+var server = "http://server.localhostsro.sk:9010/";
 
 (function () {
     // Declare app level module which depends on views, and components
